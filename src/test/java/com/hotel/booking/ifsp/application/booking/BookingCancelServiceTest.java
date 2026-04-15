505a55511b06b0d97e13541b070d0b0a563c3b2e
Bernardo_Filho <berna10.fernandes@gmail.com>
2026-04-15 03:20:19 -0300

    test(booking): add Booking cancellation test class

---
+++ src/test/java/com/hotel/booking/ifsp/application/booking/BookingCancelServiceTest.java
@@ -0,0 +1,16 @@
+package com.hotel.booking.ifsp.application.booking;
+
+import com.hotel.booking.ifsp.domain.booking.*;
+import org.junit.jupiter.api.Tag;
+
+
+@Tag("UnitTest")
+@Tag("TDD")
+class BookingCancelServiceTest {
+
+    private BookingRepository bookingRepository;
+    private BookingCancelService bookingCancelService;
+    private Booking booking;
+
+
+}
